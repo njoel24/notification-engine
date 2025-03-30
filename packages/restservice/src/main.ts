1e8f78223c5d519389b8ea456f951643f3346489
Cristiano Salvatore Anselmi <cristiano.anselmi@gmail.com>
2025-03-30 15:44:00 +0200

    chore:rename

---
+++ packages/restservice/src/main.ts
@@ -0,0 +1,49 @@
+import express from 'express';
+import { Kafka } from 'kafkajs';
+
+const kafka = new Kafka({
+  clientId: 'http-producer',
+  brokers: ['localhost:9092'],
+});
+
+const producer = kafka.producer();
+
+const startProducer = async () => {
+  await producer.connect();
+  console.log('Kafka Producer connected');
+};
+
+startProducer().catch(console.error);
+
+const host = '0.0.0.0'; 
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+const app = express();
+
+// HTTP endpoint to send notifications
+app.post('/notifications', async (req, res) => {
+  const { message } = req.body;
+
+  if (!message) {
+    return res.status(400).send({ error: 'Message is required' });
+  }
+
+  try {
+    await producer.send({
+      topic: 'notifications',
+      messages: [{ value: message }],
+    });
+    res.status(200).send({ status: 'Message sent to Kafka' });
+  } catch (error) {
+    console.error('Error sending message to Kafka:', error);
+    res.status(500).send({ error: 'Failed to send message' });
+  }
+});
+
+app.get('/heart-rest', (req, res) => {
+  res.send({ message: 'Sending heart beat from rest service' });
+});
+
+app.listen(port, host, () => {
+  console.log(`[ ready ] http://${host}:${port}`);
+});
---
+++ packages/wsservice/src/main.ts
@@ -0,0 +1,52 @@
+import express from 'express';
+import { WebSocketServer } from 'ws';
+import { Kafka } from 'kafkajs';
+
+const host = '0.0.0.0'; // Ensure it's not 'localhost'
+const port = process.env.PORT ? Number(process.env.PORT) : 8080;
+
+const app = express();
+
+// WebSocket server
+const wss = new WebSocketServer({ port: 8081 }); 
+console.log('WebSocket server running on ws://localhost:8081');
+
+// Kafka configuration
+const kafka = new Kafka({
+  clientId: 'wsservice',
+  brokers: ['localhost:9092'], // Replace with your Kafka broker(s)
+});
+
+const consumer = kafka.consumer({ groupId: 'notification-group' });
+
+const startConsumer = async () => {
+  await consumer.connect();
+  console.log('Kafka Consumer connected');
+
+  await consumer.subscribe({ topic: 'notifications', fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      const notification = message.value?.toString();
+      console.log(`Received message: ${notification}`);
+
+      wss.clients.forEach((client) => {
+        if (client.readyState === 1) {
+          client.send(notification);
+        }
+      });
+    },
+  });
+};
+
+startConsumer().catch(console.error);
+
+// HTTP endpoint for health check
+app.get('/heart-ws', (req, res) => {
+  res.send({ message: 'sending heart beat from ws microservice' });
+});
+
+
+app.listen(port, host, () => {
+  console.log(`[ ready ] http://${host}:${port}`);
+});
